224773ba7b52660f654f908dc6279798d175acbd
Preet Suthar <preetsutharxd@gmail.com>
2024-04-18 21:04:48 +0530

    Update Hero component with new title and button styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,12 @@
+import Link from "next/link";
+
 export const Hero = () => {
   return (
     <>
-      <div className="flex items-center justify-center my-16 text-center hero p-7">
+      <div className="flex flex-col items-center justify-center gap-5 py-48 overflow-x-hidden text-center px-7 hero step1">
         <div className="hero-content">
-          <h1 className="max-w-5xl font-extrabold tracking-tighter text-7xl bricolage-fonts leading-15 max-sm:text-5xl">
-            Web Components, Zero Effort
+          <h1 className="max-w-4xl font-extrabold tracking-tighter text-7xl bricolage-fonts leading-15 max-sm:text-4xl">
+            Gorgeous Web Components, Zero Effort
           </h1>
           <p className="max-w-2xl py-4 mx-auto text-sm leading-5 opacity-80">
             Elevate your projects with stunning, modern components. Simply copy
@@ -12,7 +14,14 @@ export const Hero = () => {
             awesomeness.
           </p>
         </div>
-        <div className="hero-buttons"></div>
+        <div className="flex flex-wrap gap-2 hero-buttons">
+          <Link href="/" className="primary-button grow">
+            Browse Components
+          </Link>
+          <Link href="/" className="secondary-button grow">
+            Pricing
+          </Link>
+        </div>
       </div>
     </>
   );
