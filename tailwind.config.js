87c5dfa996a249383685c579d6766160a8ba2610
Preet Suthar <preetsutharxd@gmail.com>
2024-04-19 11:44:02 +0530

    Add new font sizes to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,16 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontSize: {
+        xsm: "0.6rem",
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.25rem",
+        "2xl": "1.563rem",
+        "3xl": "1.953rem",
+        "4xl": "2.441rem",
+        "5xl": "3.052rem",
+      },
     },
   },
   plugins: [],
