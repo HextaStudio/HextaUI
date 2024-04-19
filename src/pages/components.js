0483620cd8233506656f6d4f79eaf1c99dd052b9
Preet Suthar <preetsutharxd@gmail.com>
2024-04-19 16:43:19 +0530

    Update components.js to add new web components and improve styling

--- src/pages/components.js
+++ src/pages/components.js
@@ -1,20 +1,38 @@
 import { DocsLayout } from "@/components/DocsPage/DocsLayout";
 
+import Link from "next/link";
+import Image from "next/image";
+
+const CardComponent = ({ component }) => {
+  return (
+    <>
+      <div>
+        <Image
+          src={component.image}
+          width={1920}
+          height={1080}
+          alt={component.title}
+          objectFit="cover"
+          loading="lazy"
+        />
+      </div>
+    </>
+  );
+};
+
 const components = () => {
   return (
     <>
       <DocsLayout>
         <main>
-          <h1 className="h1">
-            Copy Paste Gorgeous Web Components,Without Effort
-          </h1>
+          <h1 className="h1">Gorgeous Web Components, Zero Effort</h1>
           <p className="opacity-80">
             Just copy and paste our prebuilt components in your website and make
             your website look awesome!
           </p>
           <div className="flex flex-col gap-4 py-10">
             <div>
-              <h3 className="h3">Top picked Components</h3>
+              <h3 className="h3">Top picked components</h3>
             </div>
           </div>
         </main>
