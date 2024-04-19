a932c278512ed3d0a6e4cd997ad7ab5f607a66f9
Preet Suthar <preetsutharxd@gmail.com>
2024-04-19 15:38:11 +0530

    Update GSAP integration in DocsLayout.js and globals.css

--- src/components/DocsPage/DocsLayout.js
+++ src/components/DocsPage/DocsLayout.js
@@ -2,18 +2,51 @@ import { Navbar } from "../Navbar";
 import { Footer } from "../Footer";
 import { Sidebar } from "./Sidebar";
 
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+import { useEffect, useRef } from "react";
+
 export const DocsLayout = ({ children }) => {
+  const navbarRef = useRef(null);
+  const sidebarRef = useRef(null);
+  const footerRef = useRef(null);
+
+  useEffect(() => {
+    const pinSidebar = ScrollTrigger.create({
+      trigger: sidebarRef.current,
+      start: "top top",
+      pin: true,
+      pinSpacing: false,
+    });
+
+    return () => {
+      pinSidebar.kill();
+    };
+  }, []);
+
   return (
     <>
-      <div>
+      <div ref={navbarRef}>
         <Navbar />
       </div>
       <div className="flex">
-        <Sidebar />
+        <aside ref={sidebarRef}>
+          <Sidebar />
+        </aside>
         <main className="flex justify-center flex-grow p-10">{children}</main>
       </div>
-      <div>
-        <Footer />
+      <div
+        className="footer-div"
+        ref={footerRef}
+        style={{
+          display: "flex",
+          marginLeft: "auto",
+        }}
+      >
+        <Footer docs />
       </div>
     </>
   );
