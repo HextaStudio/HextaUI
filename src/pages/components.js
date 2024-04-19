91a9a0ae999145076d2f2e613151c75b173679b7
Preet Suthar <preetsutharxd@gmail.com>
2024-04-19 16:23:01 +0530

    Update components.js to add new web components and improve styling

--- src/pages/components.js
+++ src/pages/components.js
@@ -4,7 +4,20 @@ const components = () => {
   return (
     <>
       <DocsLayout>
-        <main className="relative"></main>
+        <main>
+          <h1 className="h1">
+            Copy Paste Gorgeous Web Components,Without Effort
+          </h1>
+          <p className="opacity-80">
+            Just copy and paste our prebuilt components in your website and make
+            your website look awesome!
+          </p>
+          <div className="flex flex-col gap-4 py-10">
+            <div>
+              <h3 className="h3">Top picked Components</h3>
+            </div>
+          </div>
+        </main>
       </DocsLayout>
     </>
   );
