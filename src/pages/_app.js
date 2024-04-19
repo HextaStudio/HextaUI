ca4fa479a4ddbf64f1aa9bd23b40533141860b75
Preet Suthar <preetsutharxd@gmail.com>
2024-04-19 11:44:53 +0530

    Update syntax highlighting configuration in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,33 @@
-import { Layout } from "@/components/Layout";
 import "@/styles/globals.css";
+import "@/styles/tokyo-night-dark.css";
+import Script from "next/script";
+import { useEffect } from "react";
+
+import hljs from "highlight.js";
+import javascript from "highlight.js/lib/languages/javascript";
+import css from "highlight.js/lib/languages/css";
+import scss from "highlight.js/lib/languages/scss";
+import shell from "highlight.js/lib/languages/shell";
+import xml from "highlight.js/lib/languages/xml";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    hljs.registerLanguage("javascript", javascript);
+    hljs.registerLanguage("shell", shell);
+    hljs.registerLanguage("scss", scss);
+    hljs.registerLanguage("css", css);
+    hljs.registerLanguage("html", xml);
+    hljs.registerLanguage("xml", xml);
+  }, []);
+
+  useEffect(() => {
+    hljs.highlightAll({ detectLanguage: false });
+  }, []);
+
   return (
     <>
+      <Script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></Script>
+
       <Component {...pageProps} />
     </>
   );
