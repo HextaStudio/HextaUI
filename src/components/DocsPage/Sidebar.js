223369f641f2d38d32ee2576e22981b8973882a1
Preet Suthar <preetsutharxd@gmail.com>
2024-04-19 11:44:40 +0530

    Update Sidebar component styles in Sidebar.js

--- src/components/DocsPage/Sidebar.js
+++ src/components/DocsPage/Sidebar.js
@@ -3,9 +3,9 @@ import Link from "next/link";
 export const Sidebar = () => {
   return (
     <>
-      <aside className="sticky top-0 flex flex-col h-full gap-4 border-r-2 border-zinc-900 p-7 sidebar min-h-dvh max-sm:hidden max-w-60">
+      <aside className="sticky top-0 flex flex-col h-full gap-4 p-10 border-r-2 border-zinc-900 sidebar min-h-dvh max-sm:hidden max-w-80">
         <div className="sidebar-section">
-          <p className="text-sm font-bold">PINNED</p>
+          <p className="text-sm font-bold uppercase">PINNED</p>
           <ul>
             <li>
               <span className="text-sm opacity-60">Stay updated </span>
@@ -14,23 +14,42 @@ export const Sidebar = () => {
                 href="https://x.com/hextastudio"
                 target="_blank"
               >
-                Twitter
+                @HextaStudio
               </Link>
             </li>
           </ul>
         </div>
         <div className="sidebar-section">
-          <p className="text-sm font-bold">Resources</p>
+          <p className="text-sm font-bold uppercase">Resources</p>
           <ul>
             <li>
-              <Link className="text-sm opacity-60" href="#">
+              <Link
+                className="text-sm transition-all opacity-60 hover:opacity-90 "
+                href="/docs/resources/install-next"
+              >
                 Install Next.js
               </Link>
             </li>
+            <li>
+              <Link
+                className="text-sm transition-all opacity-60 hover:opacity-90 "
+                href="/docs/resources/install-tailwind"
+              >
+                Install Tailwind CSS
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-sm transition-all opacity-60 hover:opacity-90 "
+                href="/docs/resources/install-tailwind"
+              >
+                Add utilities
+              </Link>
+            </li>
           </ul>
         </div>
         <div className="sidebar-section">
-          <p className="text-sm font-bold">All Components</p>
+          <p className="text-sm font-bold uppercase">All Components</p>
           <ul>
             <li>
               <Link className="text-sm opacity-60" href="#">
