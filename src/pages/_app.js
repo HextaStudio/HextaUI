79b2aa363793cbcfb89231678f64f8a67e9b13ad
Preet Suthar <preetsutharxd@gmail.com>
2024-04-20 21:03:00 +0530

    Update syntax highlighting in _app.js and remove unused script tag

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,29 +1,7 @@
 import "@/styles/globals.css";
 import "@/styles/tokyo-night-dark.css";
-import Script from "next/script";
-import { useEffect } from "react";
-
-import hljs from "highlight.js";
-import javascript from "highlight.js/lib/languages/javascript";
-import css from "highlight.js/lib/languages/css";
-import scss from "highlight.js/lib/languages/scss";
-import bash from "highlight.js/lib/languages/bash";
-import xml from "highlight.js/lib/languages/xml";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    hljs.registerLanguage("javascript", javascript);
-    hljs.registerLanguage("bash", bash);
-    hljs.registerLanguage("scss", scss);
-    hljs.registerLanguage("css", css);
-    hljs.registerLanguage("html", xml);
-    hljs.registerLanguage("xml", xml);
-  });
-
-  useEffect(() => {
-    hljs.highlightAll({ detectLanguage: true });
-  });
-
   return (
     <>
       <Component {...pageProps} />
