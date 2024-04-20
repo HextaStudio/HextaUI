a6776f69b00a09f3880e8529f59fdb00422211aa
Preet Suthar <preetsutharxd@gmail.com>
2024-04-20 20:59:34 +0530

    Refactor syntax highlighting and fix alignment issues in various files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,39 +10,23 @@ import scss from "highlight.js/lib/languages/scss";
 import bash from "highlight.js/lib/languages/bash";
 import xml from "highlight.js/lib/languages/xml";
 
-import { useRouter } from "next/router";
-
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  // useEffect(() => {
-  //   hljs.registerLanguage("javascript", javascript);
-  //   hljs.registerLanguage("bash", bash);
-  //   hljs.registerLanguage("scss", scss);
-  //   hljs.registerLanguage("css", css);
-  //   hljs.registerLanguage("html", xml);
-  //   hljs.registerLanguage("xml", xml);
-  // }, []);
+  useEffect(() => {
+    hljs.registerLanguage("javascript", javascript);
+    hljs.registerLanguage("bash", bash);
+    hljs.registerLanguage("scss", scss);
+    hljs.registerLanguage("css", css);
+    hljs.registerLanguage("html", xml);
+    hljs.registerLanguage("xml", xml);
+  });
 
-  // useEffect(() => {
-  //   hljs.highlightAll({ detectLanguage: true });
-  // }, []);
-
-  // useEffect(() => {
-  //   const handleRouteChange = () => {
-  //     hljs.highlightAll({ detectLanguage: true });
-  //   };
-
-  //   router.events.on("routeChangeComplete", handleRouteChange);
-
-  //   return () => {
-  //     router.events.off("routeChangeComplete", handleRouteChange);
-  //   };
-  // }, [router.events]);
+  useEffect(() => {
+    hljs.highlightAll({ detectLanguage: true });
+  });
 
   return (
     <>
       <Script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></Script>
-
       <Component {...pageProps} />
     </>
   );
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      
       <body>
         <Main />
         <NextScript />
