0acd23ad55c2e556faf672ad7fdc27d0012a34da
Preet Suthar <preetsutharxd@gmail.com>
2024-04-23 13:36:56 +0530

    Refactor Layout component in Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,35 +1,10 @@
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
 
-import { useEffect, useRef } from "react";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/dist/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
-
 export const Layout = ({ docs, children }) => {
-  const navbarRef = useRef(null);
-
-  useEffect(() => {
-    gsap.to(navbarRef.current, {
-      background: "#000000",
-      borderBottom: "1px solid #ffffff20",
-      scrollTrigger: {
-        scrub: true,
-        trigger: navbarRef.current,
-        start: "top",
-      },
-    });
-  }, []);
   return (
     <>
-      <div
-        className="fixed top-0 z-50 w-full border-b-zinc-800"
-        style={{
-          borderBottom: "1px solid #ffffff00",
-        }}
-        ref={navbarRef}
-      >
+      <div className="fixed top-0 z-50 w-full border-b-zinc-800">
         <Navbar />
       </div>
       <main>{children}</main>
