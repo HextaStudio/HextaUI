49fbcdb318ec2b5ce63de33ab5485fd1574e0baf
Preet Suthar <preetsutharxd@gmail.com>
2024-04-28 12:23:11 +0530

    Add Umami analytics script to _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,6 +10,11 @@ export default function App({ Component, pageProps }) {
         src="https://platform.twitter.com/widgets.js"
         charset="utf-8"
       ></Script>
+      <Script
+        defer
+        src="https://eu.umami.is/script.js"
+        data-website-id="de2f88ed-b01d-46df-8a45-e3443a7a69a5"
+      ></Script>
       <Component {...pageProps} />
     </>
   );
