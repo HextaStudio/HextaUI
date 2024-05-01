c47b62f5a93dfc8988e76cbe2b4b30078d21f17b
Preet Suthar <preetsutharxd@gmail.com>
2024-05-01 14:05:13 +0530

    Add more variants in button component

--- src/pages/docs/components/action/buttons.js
+++ src/pages/docs/components/action/buttons.js
@@ -47,6 +47,110 @@ export const ButtonsPreview = () => {
   );
 };
 
+export const DisabledButtonsPreview = () => {
+  return (
+    <>
+      <div className="flex flex-wrap items-center gap-4 m-5">
+        {/* primary */}
+        <button
+          disabled
+          className="px-[20px] py-[8px] bg-white border border-zinc-900 text-black rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90 disabled:contrast-[60%] cursor-not-allowed"
+        >
+          Primary
+        </button>
+
+        {/* secondary */}
+        <button
+          disabled
+          className="px-[20px] py-[8px] bg-zinc-900  border border-zinc-900 text-white rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90 disabled:contrast-[60%] cursor-not-allowed"
+        >
+          Secondary
+        </button>
+
+        {/* info */}
+        <button
+          disabled
+          className="px-[20px] py-[8px] bg-blue-400  border border-blue-900 text-black rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90 disabled:contrast-[60%] cursor-not-allowed"
+        >
+          Info
+        </button>
+
+        {/* Success */}
+        <button
+          disabled
+          className="px-[20px] py-[8px] bg-green-400   border border-green-900 text-black rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90 disabled:contrast-[60%] cursor-not-allowed"
+        >
+          Success
+        </button>
+
+        {/* Fail */}
+        <button
+          disabled
+          className="px-[20px] py-[8px] bg-red-700  border border-red-900 text-white rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90 disabled:contrast-[60%] cursor-not-allowed"
+        >
+          Failure
+        </button>
+      </div>
+    </>
+  );
+};
+
+export const OutlinedButtonsPreview = () => {
+  return (
+    <>
+      <div className="flex flex-wrap items-center gap-4 m-5">
+        {/* primary */}
+        <button className="px-[20px] py-[8px] border-2 border-zinc-900 text-zinc-100 rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+          Primary
+        </button>
+
+        {/* info */}
+        <button className="px-[20px] py-[8px] border-2 border-blue-900 text-blue-100 rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+          Info
+        </button>
+
+        {/* Success */}
+        <button className="px-[20px] py-[8px] border-2 border-green-900 text-green-100 rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+          Success
+        </button>
+
+        {/* Fail */}
+        <button className="px-[20px] py-[8px] border-2 border-red-900 text-red-100 rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+          Failure
+        </button>
+      </div>
+    </>
+  );
+};
+
+export const OutlinedWithHoverButtonsPreview = () => {
+  return (
+    <>
+      <div className="flex flex-wrap items-center gap-4 m-5">
+        {/* primary */}
+        <button className="px-[20px] py-[8px] border-2 border-zinc-900 text-zinc-100 hover:bg-white hover:text-black rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+          Primary
+        </button>
+
+        {/* info */}
+        <button className="px-[20px] py-[8px] border-2 border-blue-900 text-blue-100 hover:bg-blue-500 hover:text-white rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+          Info
+        </button>
+
+        {/* Success */}
+        <button className="px-[20px] py-[8px] border-2 border-green-900 text-green-100 hover:bg-green-400 hover:text-white rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+          Success
+        </button>
+
+        {/* Fail */}
+        <button className="px-[20px] py-[8px] border-2 border-red-900 text-red-100 hover:bg-red-700 hover:text-white rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+          Failure
+        </button>
+      </div>
+    </>
+  );
+};
+
 const buttons = () => {
   return (
     <>
@@ -129,6 +233,134 @@ const buttons = () => {
 {/* Link */}
 <button className="px-[20px] py-[8px]  text-white rounded-lg flex items-center font-[600] text-[14px] hover:underline underline-offset-1 transition-all duration-[0.4s]">
   Link
+</button>`}
+              />
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-col gap-4 py-10 preview">
+              <div>
+                <h3 className="h3">Disabled Buttons</h3>
+                <div className="relative flex items-center justify-center my-3 overflow-hidden border border-white border-opacity-10 rounded-2xl preview-container">
+                  <DisabledButtonsPreview />
+                </div>
+              </div>
+            </div>
+            <div className="installation">
+              <h3 className="h3">Code</h3>
+              <CodeBlock
+                lang="jsx"
+                filename="jsx"
+                code={`{/* primary */}
+<button
+  disabled
+  className="px-[20px] py-[8px] bg-white border border-zinc-900 text-black rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90 disabled:contrast-[60%] cursor-not-allowed"
+>
+  Primary
+</button>
+
+{/* secondary */}
+<button
+  disabled
+  className="px-[20px] py-[8px] bg-zinc-900  border border-zinc-900 text-white rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90 disabled:contrast-[60%] cursor-not-allowed"
+>
+  Secondary
+</button>
+
+{/* info */}
+<button
+  disabled
+  className="px-[20px] py-[8px] bg-blue-400  border border-blue-900 text-black rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90 disabled:contrast-[60%] cursor-not-allowed"
+>
+  Info
+</button>
+
+{/* Success */}
+<button
+  disabled
+  className="px-[20px] py-[8px] bg-green-400   border border-green-900 text-black rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90 disabled:contrast-[60%] cursor-not-allowed"
+>
+  Success
+</button>
+
+{/* Fail */}
+<button
+  disabled
+  className="px-[20px] py-[8px] bg-red-700  border border-red-900 text-white rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90 disabled:contrast-[60%] cursor-not-allowed"
+>
+  Failure
+</button>`}
+              />
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-col gap-4 py-10 preview">
+              <div>
+                <h3 className="h3">Outlined Buttons</h3>
+                <div className="relative flex items-center justify-center my-3 overflow-hidden border border-white border-opacity-10 rounded-2xl preview-container">
+                  <OutlinedButtonsPreview />
+                </div>
+              </div>
+            </div>
+            <div className="installation">
+              <h3 className="h3">Code</h3>
+              <CodeBlock
+                lang="jsx"
+                filename="jsx"
+                code={`{/* primary */}
+<button className="px-[20px] py-[8px] border-2 border-zinc-900 text-zinc-100 rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+  Primary
+</button>
+
+{/* info */}
+<button className="px-[20px] py-[8px] border-2 border-blue-900 text-blue-100 rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+  Info
+</button>
+
+{/* Success */}
+<button className="px-[20px] py-[8px] border-2 border-green-900 text-green-100 rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+  Success
+</button>
+
+{/* Fail */}
+<button className="px-[20px] py-[8px] border-2 border-red-900 text-red-100 rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+  Failure
+</button>`}
+              />
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-col gap-4 py-10 preview">
+              <div>
+                <h3 className="h3">Outlined With Background Buttons</h3>
+                <div className="relative flex items-center justify-center my-3 overflow-hidden border border-white border-opacity-10 rounded-2xl preview-container">
+                  <OutlinedWithHoverButtonsPreview />
+                </div>
+              </div>
+            </div>
+            <div className="installation">
+              <h3 className="h3">Code</h3>
+              <CodeBlock
+                lang="jsx"
+                filename="jsx"
+                code={`{/* primary */}
+<button className="px-[20px] py-[8px] border-2 border-zinc-900 text-zinc-100 hover:bg-white hover:text-black rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+  Primary
+</button>
+
+{/* info */}
+<button className="px-[20px] py-[8px] border-2 border-blue-900 text-blue-100 hover:bg-blue-500 hover:text-white rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+  Info
+</button>
+
+{/* Success */}
+<button className="px-[20px] py-[8px] border-2 border-green-900 text-green-100 hover:bg-green-400 hover:text-white rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+  Success
+</button>
+
+{/* Fail */}
+<button className="px-[20px] py-[8px] border-2 border-red-900 text-red-100 hover:bg-red-700 hover:text-white rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90">
+  Failure
 </button>`}
               />
             </div>
