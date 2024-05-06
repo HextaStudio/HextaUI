cae33c9092397f5a1f7fb089975f453d589bfa1a
Preet Suthar <preetsutharxd@gmail.com>
2024-05-06 21:32:53 +0530

    feat: Add async attribute to Twitter script tag in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -49,6 +49,7 @@ export default function App({ Component, pageProps }) {
         ]}
       />
       <Script
+        async
         src="https://platform.twitter.com/widgets.js"
         charset="utf-8"
       ></Script>
