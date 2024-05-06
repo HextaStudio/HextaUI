215cce624a8cc688a64a92fb60e78c95be5f099d
Preet Suthar <preetsutharxd@gmail.com>
2024-05-06 11:39:24 +0530

    feat: Add @cloudflare/next-on-pages npm dependency for building pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,8 @@ import { NextSeo } from "next-seo";
 
 import "@splidejs/react-splide/css";
 
+export const runtime = "edge";
+
 export default function App({ Component, pageProps }) {
   return (
     <>
