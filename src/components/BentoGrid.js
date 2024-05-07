4c832a69ef1847879c416cc0da1d4a7c5711e2e3
Preet Suthar <preetsutharxd@gmail.com>
2024-05-07 07:55:52 +0530

    refactor: Update Image import to use next/image

--- src/components/BentoGrid.js
+++ src/components/BentoGrid.js
@@ -1,4 +1,4 @@
-import Image from "next/legacy/image";
+import Image from "next/image";
 
 import bento3 from "../../public/assets/bento-3.png";
 import bento2 from "../../public/assets/bento-2.png";
