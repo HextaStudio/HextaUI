67d6b79b99e71efd9bfebd75c2ff509686c6bcad
Preet Suthar <preetsutharxd@gmail.com>
2024-05-09 10:57:34 +0530

    refactor: Update Loader components in CLI code to use ES modules and optimize
    This commit updates the Loader components in the CLI code to use ES modules instead of CommonJS modules, improving code readability and maintainability. It also optimizes the Loader components by specifying their size using pixel values instead of dynamic classes, ensuring consistent rendering across different screen sizes.

--- hexta-ui/cli/components/Loader.js
+++ hexta-ui/cli/components/Loader.js
@@ -5,58 +5,51 @@ const cn = (...args) => {
   return twMerge(clsx(args));
 };
 
-export const SpinnerCircleLoader = ({ className }) => {
+export const SpinnerCircleLoader = ({ className, ...props }) => {
   return (
-    <>
+    <div
+      className={cn(
+        "w-[30px] h-[30px] border-t-2 rounded-full border-zinc animate-spin",
+        className
+      )}
+      {...props}
+    ></div>
+  );
+};
+
+export const SpinnerSquareLoader = ({ className, ...props }) => {
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className={cn("w-[30px] h-[30px] bg-zinc-100 animate-spin", className)}
+        {...props}
+      ></div>
+    </div>
+  );
+};
+
+export const PingLoader = ({ className, ...props }) => {
+  return (
+    <div className="flex items-center justify-center h-screen">
       <div
         className={cn(
-          "w-[30px] h-[30px] border-t-2 rounded-full border-zinc animate-spin",
+          "w-[30px] h-[30px] rounded-full bg-zinc-100 animate-ping",
           className
         )}
+        {...props}
       ></div>
-    </>
+    </div>
   );
 };
 
-export const SpinnerSquareLoader = ({ className }) => {
+export const ColoredLoader = ({ className, ...props }) => {
   return (
-    <>
-      <div className="flex items-center justify-center">
-        <div
-          className={cn(
-            "w-[30px] h-[30px] bg-zinc-100 animate-spin",
-            className
-          )}
-        ></div>
-      </div>
-    </>
-  );
-};
-
-export const PingLoader = ({ className }) => {
-  return (
-    <>
-      <div className="flex items-center justify-center h-screen">
-        <div
-          className={cn(
-            "w-[30px] h-[30px] rounded-full bg-zinc-100 animate-ping",
-            className
-          )}
-        ></div>
-      </div>
-    </>
-  );
-};
-
-export const ColoredLoader = ({ className }) => {
-  return (
-    <>
-      <div
-        className={cn(
-          "w-[30px] h-[30px] border-zinc-500 border-t-2 rounded-full border-zinc animate-spin",
-          className
-        )}
-      ></div>
-    </>
+    <div
+      className={cn(
+        "w-[30px] h-[30px] border-zinc-500 border-t-2 rounded-full border-zinc animate-spin",
+        className
+      )}
+      {...props}
+    ></div>
   );
 };
