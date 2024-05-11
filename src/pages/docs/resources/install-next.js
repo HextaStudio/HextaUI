d8bcc3aed8666a7ed1ef959a174254b74b36f668
Preet Suthar <preetsutharxd@gmail.com>
2024-05-11 13:45:22 +0530

    refactor: Update install-next.js to use Tailwind CSS and src/ directory
    The commit modifies the install-next.js file to update the project setup prompts. It removes the prompts for TypeScript and ESLint usage, and adds prompts for using Tailwind CSS and the src/ directory. This change aligns the project setup with the desired configuration and improves the developer experience by providing a streamlined setup process.

--- src/pages/docs/resources/install-next.js
+++ src/pages/docs/resources/install-next.js
@@ -48,13 +48,12 @@ const installNext = () => {
                 filename="bash"
                 lang="bash"
                 code={`What is your project named? my-app
-Would you like to use TypeScript? No / Yes
-Would you like to use ESLint? No / Yes
-Would you like to use Tailwind CSS? No / Yes
-Would you like to use src/ directory? No / Yes
+Would you like to use TypeScript? No 
+Would you like to use ESLint? No 
+Would you like to use Tailwind CSS? Yes
+Would you like to use src/ directory? Yes
 Would you like to use App Router? (recommended) No / Yes
-Would you like to customize the default import alias (@/*)? No / Yes
-What import alias would you like configured? @/*
+Would you like to customize the default import alias (@/*)? No
 `}
               />
             </div>
