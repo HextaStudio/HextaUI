4da8f1a50aad2f14c1d9687ae6c25ae140b3d4c1
Preet Suthar <preetsutharxd@gmail.com>
2024-05-18 21:16:05 +0530

    feat: Add DocsPreview component for displaying document previews

---
+++ src/components/utils/DocsPreview.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const cn = (...args: any[]) => {
+  return clsx(twMerge(...args));
+};
+interface DocsPreviewProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const DocsPreview = ({ children, className }: DocsPreviewProps) => {
+  return (
+    <>
+      <div>
+        <div className="flex flex-col gap-4 py-10 preview">
+          <div>
+            <h3 className="h3">Preview</h3>
+            <div
+              className={cn(
+                "relative flex items-center justify-center my-3 overflow-hidden border border-white border-opacity-10 rounded-2xl preview-container h-[15rem]",
+                className
+              )}
+            >
+              {children}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
