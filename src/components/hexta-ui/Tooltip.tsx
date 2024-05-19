eec1719472bb45a6a45765667a8d2f396a1817cb
Preet Suthar <preetsutharxd@gmail.com>
2024-05-19 10:05:11 +0530

    refactor: Update Tooltip component to use React context for visibility state

--- src/components/hexta-ui/Tooltip.tsx
+++ src/components/hexta-ui/Tooltip.tsx
@@ -1,30 +1,65 @@
-import { useState } from "react";
+import { useState, createContext, useContext } from "react";
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
-const cn = (...args: (string | undefined)[]): string => {
-  return twMerge(clsx(args));
+const cn = (...args: any[]) => {
+  return clsx(twMerge(...args));
 };
-
-interface TooltipProps {
-  text: string;
-  direction?: "top" | "bottom" | "left" | "right";
-  children: React.ReactNode;
-  className?: string;
+interface TooltipContextProps {
+  isVisible: boolean;
+  toggleVisibility: () => void;
 }
 
-export const Tooltip = ({
-  text,
-  direction = "top",
-  children,
-  className,
-}: TooltipProps) => {
-  const [isVisible, setIsVisible] = useState(false);
+const TooltipContext = createContext<TooltipContextProps | undefined>(
+  undefined
+);
 
+interface TooltipProviderProps {
+  children: React.ReactNode;
+}
+
+export const TooltipProvider = ({ children }: TooltipProviderProps) => {
+  const [isVisible, setIsVisible] = useState(false);
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
   };
 
+  return (
+    <TooltipContext.Provider value={{ isVisible, toggleVisibility }}>
+      {children}
+    </TooltipContext.Provider>
+  );
+};
+
+interface TooltipTriggerProps {
+  children: React.ReactNode;
+}
+
+export const TooltipTrigger = ({ children }: TooltipTriggerProps) => {
+  const tooltipContext = useContext(TooltipContext);
+  const { toggleVisibility } = tooltipContext || {};
+  return (
+    <div onMouseEnter={toggleVisibility} onMouseLeave={toggleVisibility}>
+      {children}
+    </div>
+  );
+};
+
+interface TooltipContentProps {
+  direction?: "top" | "bottom" | "left" | "right";
+  className?: string;
+  children: React.ReactNode;
+  props?: any;
+}
+
+export const TooltipContent: React.FC<TooltipContentProps> = ({
+  direction = "top",
+  className,
+  children,
+  props,
+}) => {
+  const tooltipContext = useContext(TooltipContext);
+  const { isVisible } = tooltipContext || {};
   const directionClasses: { [key: string]: string } = {
     top: "bottom-full left-1/2 -translate-x-1/2 mb-2",
     bottom: "top-full left-1/2 -translate-x-1/2 mt-2",
@@ -32,18 +67,28 @@ export const Tooltip = ({
     right: "left-full top-1/2 -translate-y-1/2 ml-2",
   };
 
-  const tooltipClasses = `absolute z-10  px-3 py-2 text-xs font-medium text-black bg-white rounded-md whitespace-nowrap ${
-    directionClasses[direction]
-  } border-zinc-800 border transform transition-all duration-[0.2s] flex ${
-    isVisible ? "flex" : "hidden"
-  }`;
-
   return (
-    <div className="relative flex">
-      <div onMouseEnter={toggleVisibility} onMouseLeave={toggleVisibility}>
-        {children}
-      </div>
-      <div className={cn(tooltipClasses, className)}>{text}</div>
+    <div
+      className={cn(
+        `absolute z-10 px-3 py-2 text-xs font-medium text-black bg-white rounded-md whitespace-nowrap border-zinc-800 border transform transition-all duration-[0.2s] flex   ${
+          directionClasses[direction]
+        } ${isVisible ? "opacity-100" : "opacity-0"} ${
+          isVisible ? "scale-[1]" : "scale-[0.9]"
+        }`,
+        className
+      )}
+      {...props}
+    >
+      {children}
     </div>
   );
+};
+
+interface TooltipProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Tooltip = ({ children, className }: TooltipProps) => {
+  return <div className={cn("relative flex", className)}>{children}</div>;
 };
--- src/pages/docs/components/layout/Tooltip.tsx
+++ src/pages/docs/components/layout/Tooltip.tsx
@@ -2,47 +2,20 @@ import { DocsLayout } from "@/components/DocsPage/DocsLayout";
 import { CodeBlock } from "@/components/DocsPage/CodeBlock";
 import React from "react";
 
-import { Tooltip } from "@/components/hexta-ui/Tooltip";
+import {
+  Tooltip,
+  TooltipTrigger,
+  TooltipProvider,
+  TooltipContent,
+} from "@/components/hexta-ui/Tooltip";
 
 import { Badge } from "@/components/hexta-ui/Badge";
-import { Table } from "@/components/hexta-ui/Table";
 import { InstallationSteps } from "@/components/utils/InstallationSteps";
 import { DocsHeader } from "@/components/utils/DocsHeader";
 import { DocsSEO } from "@/components/DocsPage/DocsSEO";
+import { DocsPreview } from "@/components/utils/DocsPreview";
 
 const tooltip = () => {
-  const data = [
-    {
-      propName: "direction",
-      propType: "string",
-      defaultValue: "top",
-      description: "Direction of the tooltip.",
-    },
-    {
-      propName: "text",
-      propType: "string",
-      defaultValue: "null",
-      description: "Text to be displayed in the tooltip.",
-    },
-  ];
-  const columns = [
-    {
-      label: "Prop Name",
-      key: "propName",
-    },
-    {
-      label: "Prop Type",
-      key: "propType",
-    },
-    {
-      label: "Default Value",
-      key: "defaultValue",
-    },
-    {
-      label: "Description",
-      key: "description",
-    },
-  ];
   return (
     <>
       <DocsSEO
@@ -56,45 +29,46 @@ const tooltip = () => {
             title="Tooltip"
             description="An awesome component to display tooltip for information."
           />
-          <div>
-            <div className="flex flex-col gap-4 py-10 preview">
-              <div>
-                <h3 className="h3">Preview</h3>
-                <div className="relative flex items-center justify-center my-3 overflow-hidden border border-white border-opacity-10 rounded-2xl preview-container h-[15rem]">
-                  <Tooltip direction="top" text="Meow ~">
-                    <Badge outlined className="cursor-default">
-                      🐈 Hover me
-                    </Badge>
-                  </Tooltip>
-                </div>
-              </div>
-            </div>
-          </div>
-          <InstallationSteps component="Tooltip" />
-          <div>
-            <div className="relative flex items-center justify-center my-3 overflow-hidden border border-white border-opacity-10 rounded-2xl preview-container h-[15rem]">
-              <Tooltip direction="top" text="Meow ~">
-                <Badge outlined className="cursor-default">
-                  🐈 Hover me
-                </Badge>
+          <DocsPreview>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <p className="px-3 py-1 border-2 border-white rounded-full cursor-default text-md border-opacity-30">
+                    Hover me
+                  </p>
+                </TooltipTrigger>
+                <TooltipContent direction="top">
+                  <p className="text-sm">This is content of tooltip</p>
+                </TooltipContent>
               </Tooltip>
-            </div>
+            </TooltipProvider>
+          </DocsPreview>
+          <InstallationSteps
+            component="Tooltip"
+            imports={[
+              "Tooltip",
+              "TooltipTrigger",
+              "TooltipProvider",
+              "TooltipContent",
+            ]}
+          />
+          <div>
             <h3 className="h3">Usage</h3>
             <CodeBlock
               lang="tsx"
               filename="Home.tsx"
-              code={`<Tooltip direction="top" text="Meow ~">
-  <Badge outlined className="cursor-default">
-    🐈 Hover me
-  </Badge>
-</Tooltip>`}
-            />
-          </div>
-          <div className="mt-[5rem]">
-            <Table
-              columns={columns}
-              data={data}
-              tableTitle="Props Information"
+              code={`<TooltipProvider>
+  <Tooltip>
+    <TooltipTrigger>
+      <p className="px-3 py-1 border-2 border-white rounded-full cursor-default text-md border-opacity-30">
+      Hover me
+      </p>
+    </TooltipTrigger>
+    <TooltipContent direction="top">
+      <p className="text-sm">This is content of tooltip</p>
+    </TooltipContent>
+  </Tooltip>
+</TooltipProvider>`}
             />
           </div>
         </main>
