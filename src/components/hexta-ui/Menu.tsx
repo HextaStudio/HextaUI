e2e81736dac43542f84759451096974094e55205
preetsuthar17 <preetsutharxd@gmail.com>
2024-05-20 20:45:09 +0530

    refactor: Add framer-motion animation to Menu component

--- src/components/hexta-ui/Menu.tsx
+++ src/components/hexta-ui/Menu.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
+import { AnimatePresence, motion } from "framer-motion";
 
 const cn = (...args: any[]) => {
   return twMerge(clsx(args));
@@ -32,35 +33,25 @@ export const Menu = ({
 }: MenuProps) => {
   return (
     <div className="relative">
-      {isOpen && (
-        <div
-          className={cn(
-            "absolute right-0 w-48 mt-2 rounded-lg shadow-lg bg-zinc-950 ring-1 ring-black ring-opacity-5",
-            className
-          )}
-        >
-          <div
-            className="flex flex-col gap-2 py-2"
-            role="menu"
-            aria-orientation="vertical"
-            aria-labelledby="options-menu"
-          >
-            {items.map((item, index) => (
-              <Link
-                key={index}
-                href={item.href}
-                className="px-4 py-2 mx-2 text-[14px] font-medium transition-all rounded hover:bg-zinc-800 flex items-center gap-3"
-                role="menuitem"
-              >
-                {item.icon}
-                {item.label}
-              </Link>
-            ))}
-            {dividedItems && dividedItems.length > 0 && (
-              <hr className="opacity-30" />
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.9 }}
+            transition={{ duration: 0.3, ease: "anticipate" }}
+            className={cn(
+              "absolute right-0 w-48 mt-2 border border-white border-opacity-10 rounded-lg shadow-lg bg-zinc-950 ring-1 ring-black ring-opacity-5",
+              className
             )}
-            {dividedItems &&
-              dividedItems.map((item, index) => (
+          >
+            <div
+              className="flex flex-col gap-2 py-2"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="options-menu"
+            >
+              {items.map((item, index) => (
                 <Link
                   key={index}
                   href={item.href}
@@ -71,9 +62,25 @@ export const Menu = ({
                   {item.label}
                 </Link>
               ))}
-          </div>
-        </div>
-      )}
+              {dividedItems && dividedItems.length > 0 && (
+                <hr className="opacity-30" />
+              )}
+              {dividedItems &&
+                dividedItems.map((item, index) => (
+                  <Link
+                    key={index}
+                    href={item.href}
+                    className="px-4 py-2 mx-2 text-[14px] font-medium transition-all rounded hover:bg-zinc-800 flex items-center gap-3"
+                    role="menuitem"
+                  >
+                    {item.icon}
+                    {item.label}
+                  </Link>
+                ))}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
