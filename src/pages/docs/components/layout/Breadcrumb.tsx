64189c11d9f7d3125ab5e8fa76eb594e91ceace8
preetsuthar17 <preetsutharxd@gmail.com>
2024-05-20 07:07:46 +0530

    feat: Add DocsPreview component to Breadcrumb for displaying document previews

--- src/pages/docs/components/layout/Breadcrumb.tsx
+++ src/pages/docs/components/layout/Breadcrumb.tsx
@@ -7,6 +7,7 @@ import { Table } from "@/components/hexta-ui/Table";
 import { InstallationSteps } from "@/components/DocsPage/InstallationSteps";
 import { DocsHeader } from "@/components/DocsPage/DocsHeader";
 import { DocsSEO } from "@/components/DocsPage/DocsSEO";
+import { DocsPreview } from "@/components/DocsPage/DocsPreview";
 
 const breadcrumb = () => {
   const items = [
@@ -58,16 +59,12 @@ const breadcrumb = () => {
             title="Breadcrumb"
             description="Displays the path to the current resource using a hierarchy of links."
           />
-          <div>
-            <div className="flex flex-col gap-4 py-10 preview">
-              <div>
-                <h3 className="h3">Preview</h3>
-                <div className="relative flex items-center justify-center my-3 overflow-hidden border border-white border-opacity-10 rounded-2xl preview-container h-[13rem] p-4">
-                  <Breadcrumb items={items} />
-                </div>
-              </div>
+
+          <DocsPreview>
+            <div className="relative flex items-center justify-center my-3 overflow-hidden border border-white border-opacity-10 rounded-2xl preview-container h-[15rem] text-center">
+              <Breadcrumb items={items} />
             </div>
-          </div>
+          </DocsPreview>
           <InstallationSteps component="Breadcrumb" />
           <div>
             <h3 className="h3">Breadcrumb</h3>
