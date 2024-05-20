ea3ed9e120c950b37d7ae27dcd964b5b8a4c7e31
preetsuthar17 <preetsutharxd@gmail.com>
2024-05-20 19:50:36 +0530

    feat: Add DocsPreview component to Navbar for displaying document previews

--- src/pages/docs/components/layout/Navbar.tsx
+++ src/pages/docs/components/layout/Navbar.tsx
@@ -8,6 +8,7 @@ import { Navbar } from "@/components/hexta-ui/Navbar";
 import { InstallationSteps } from "@/components/DocsPage/InstallationSteps";
 import { DocsHeader } from "@/components/DocsPage/DocsHeader";
 import { DocsSEO } from "@/components/DocsPage/DocsSEO";
+import { DocsPreview } from "@/components/DocsPage/DocsPreview";
 
 const navbar = () => {
   const data = [
@@ -93,28 +94,25 @@ const navbar = () => {
             title="Navbar"
             description="Collection of Navbar components with different variants."
           />
-          <div className="flex flex-col gap-4 py-10 preview">
-            <div>
-              <h3 className="h3">Preview</h3>
-              <div className="relative flex items-center justify-center my-3 overflow-hidden border border-white border-opacity-10 rounded-2xl preview-container">
-                {" "}
-                <Navbar
-                  image={{
-                    src: "https://placeholder.co/100",
-                    alt: "logo",
-                    width: 45,
-                    height: 45,
-                  }}
-                  links={[
-                    { label: "Home", href: "#" },
-                    { label: "About", href: "#" },
-                    { label: "Contact", href: "#" },
-                  ]}
-                />
-              </div>
-            </div>
-          </div>
-          <InstallationSteps component="Navbar" />
+          <DocsPreview>
+            <Navbar
+              image={{
+                src: "https://placeholder.co/100",
+                alt: "logo",
+                width: 45,
+                height: 45,
+              }}
+              links={[
+                { label: "Home", href: "#" },
+                { label: "About", href: "#" },
+                { label: "Contact", href: "#" },
+              ]}
+            />
+          </DocsPreview>
+          <InstallationSteps
+            component={["Navbar", "Input"]}
+            imports={["Navbar", "Input"]}
+          />
           <div className="flex flex-col gap-4 py-10 preview">
             <div>
               <h3 className="h3">Navbar with Title</h3>
