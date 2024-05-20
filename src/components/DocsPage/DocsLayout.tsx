e8346b5cd7f98f464089ae43ceebf644fe85f75e
preetsuthar17 <preetsutharxd@gmail.com>
2024-05-20 20:54:22 +0530

    refactor: Update main layout in DocsPage to adjust margin and padding

--- src/components/DocsPage/DocsLayout.tsx
+++ src/components/DocsPage/DocsLayout.tsx
@@ -21,7 +21,7 @@ export const DocsLayout = ({ children }: DocsLayoutProps) => {
         <aside className="pt-16">
           <Sidebar />
         </aside>
-        <main className="flex flex-wrap justify-center flex-grow p-[5rem] pt-24 ml-[18rem] max-[900px]:ml-0 max-[900px]:mt-[2rem] max-[600px]:px-[2rem]">
+        <main className="flex flex-wrap justify-center flex-grow p-[5rem] mt-[2rem] ml-[18rem] max-[900px]:ml-0 max-[900px]:mt-[4rem] max-[600px]:px-[2rem]">
           {children}
         </main>
       </div>
