9302c80f760a535da7454138cee81d69c04fac3d
preetsuthar17 <preetsutharxd@gmail.com>
2024-05-21 07:15:07 +0530

    refactor: Add framer-motion animation to AlertDialog component

--- src/components/hexta-ui/AlertDialog.tsx
+++ src/components/hexta-ui/AlertDialog.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
+import { AnimatePresence, motion } from "framer-motion";
 
 const cn = (...args: any[]) => {
   return twMerge(clsx(args));
@@ -24,35 +25,43 @@ export const AlertDialog = ({
 }: AlertDialogProps) => {
   return (
     <>
-      {isOpen && (
-        <div className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center w-screen h-screen bg-black alert-dialog-backdrop z-[9999] bg-opacity-70">
-          <div
-            className={cn(
-              "flex flex-col gap-3 px-10 bg-black border border-b-2 rounded-lg alert-dialog p-7 border-zinc-900",
-              className
-            )}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.9 }}
+            transition={{ duration: 0.2, ease: "anticipate" }}
+            className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center w-screen h-screen bg-black alert-dialog-backdrop z-[9999] bg-opacity-70"
           >
-            <div>
-              <p className="text-2xl font-bold tracking-tight">{title}</p>
-              <p className="text-sm opacity-80">{message}</p>
+            <div
+              className={cn(
+                "flex flex-col gap-3 px-10 bg-black border border-b-2 rounded-lg alert-dialog p-7 border-zinc-900",
+                className
+              )}
+            >
+              <div>
+                <p className="text-2xl font-bold tracking-tight">{title}</p>
+                <p className="text-sm opacity-80">{message}</p>
+              </div>
+              <div className="flex gap-3">
+                <button
+                  className="px-[20px] py-[8px] bg-white border border-zinc-900 text-black rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90"
+                  onClick={onConfirm}
+                >
+                  Continue
+                </button>
+                <button
+                  className="px-[20px] py-[8px] bg-zinc-950 bg-opacity-20 border border-zinc-900 text-white rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90"
+                  onClick={onCancel}
+                >
+                  Cancel
+                </button>
+              </div>
             </div>
-            <div className="flex gap-3">
-              <button
-                className="px-[20px] py-[8px] bg-white border border-zinc-900 text-black rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90"
-                onClick={onConfirm}
-              >
-                Continue
-              </button>
-              <button
-                className="px-[20px] py-[8px] bg-zinc-950 bg-opacity-20 border border-zinc-900 text-white rounded-lg flex items-center font-[600] text-[14px] transition-all duration-[0.1s] hover:brightness-90"
-                onClick={onCancel}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
