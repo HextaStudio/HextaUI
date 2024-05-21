ba2fca397d652b58007798e7d3d52cfc2b375a2f
preetsuthar17 <preetsutharxd@gmail.com>
2024-05-21 07:15:19 +0530

    refactor: Update transition duration in Menu component

--- src/components/hexta-ui/Menu.tsx
+++ src/components/hexta-ui/Menu.tsx
@@ -55,7 +55,7 @@ export const Menu = ({
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.9 }}
-            transition={{ duration: 0.3, ease: "anticipate" }}
+            transition={{ duration: 0.2, ease: "anticipate" }}
             className={cn(
               "absolute right-0 w-48 mt-2 border border-white border-opacity-10 rounded-lg shadow-lg bg-zinc-950 ring-1 ring-black ring-opacity-5",
               className
