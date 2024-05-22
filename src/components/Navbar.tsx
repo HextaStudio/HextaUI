c2b0ab9b55292ac18b7291a2cf7131f84fb8518f
preetsuthar17 <preetsutharxd@gmail.com>
2024-05-22 19:07:26 +0530

    refactor: Update Navbar component with correct link to Accordion in Sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -64,7 +64,7 @@ export const Navbar = ({ docs }: NavbarProps) => {
               </li>
               <li>
                 <Link
-                  href="/docs/components/layout/AlertDialog"
+                  href="/docs/components/layout/Accordion"
                   className="opacity-80 hover:opacity-100 hover:underline transition-all"
                 >
                   Components
