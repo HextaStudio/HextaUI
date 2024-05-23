5d3b616ee10f0ef07bb4958cc5d1fc5e6c989e27
preetsuthar17 <preetsutharxd@gmail.com>
2024-05-23 07:03:39 +0530

    refactor: Update Toggle component to use DocsPreview for rendering toggle switch and state text

--- src/pages/docs/components/layout/Toggle.tsx
+++ src/pages/docs/components/layout/Toggle.tsx
@@ -9,6 +9,7 @@ import { Toggle } from "@/components/hexta-ui/Toggle";
 import { InstallationSteps } from "@/components/DocsPage/InstallationSteps";
 import { DocsHeader } from "@/components/DocsPage/DocsHeader";
 import { DocsSEO } from "@/components/DocsPage/DocsSEO";
+import { DocsPreview } from "@/components/DocsPage/DocsPreview";
 
 const toggle = () => {
   const data = [
@@ -63,36 +64,17 @@ const toggle = () => {
             title="Toggle"
             description="Toggle switch component with enabled and disabled state."
           />
-          <div>
-            <div className="flex flex-col gap-4 py-10 preview">
-              <div>
-                <h3 className="h3">Preview</h3>
-                <div className="relative flex items-center justify-center my-3 overflow-hidden border border-white border-opacity-10 rounded-2xl preview-container h-[15rem] text-center flex-col">
-                  <Toggle enabled={isEnabled} onChange={handleToggle} />
-                  <p className="mt-2 text-sm ">
-                    {isEnabled ? "Enabled" : "Disabled"}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <DocsPreview>
+            <Toggle enabled={isEnabled} onChange={handleToggle} />
+            <p className="mt-2 text-sm ">
+              {isEnabled ? "Enabled" : "Disabled"}
+            </p>
+          </DocsPreview>
           <InstallationSteps component="Toggle" />
-          <div>
-            <div className="relative flex items-center justify-center my-3 overflow-hidden border border-white border-opacity-10 rounded-2xl preview-container h-[15rem] text-center">
-              <div>
-                <Toggle enabled={isEnabled} onChange={handleToggle} />
-                <p className="mt-2 text-sm ">
-                  {isEnabled ? "Enabled" : "Disabled"}
-                </p>
-              </div>
-            </div>
-            <h3 className="h3">Usage</h3>
-            <CodeBlock
-              lang="tsx"
-              filename="Home.tsx"
-              code={`import { useState } from "react";
-import { Toggle } from "@/components/hexta-ui/Toggle";
-
+          <CodeBlock
+            lang="tsx"
+            filename="Home.tsx"
+            code={`import { useState } from "react";
 
 export const Home = () => {
   const [isEnabled, setIsEnabled] = useState(false);
@@ -113,8 +95,7 @@ export const Home = () => {
     </>
   );
 }`}
-            />
-          </div>
+          />
           <div className="mt-[5rem]">
             <Table
               columns={columns}
