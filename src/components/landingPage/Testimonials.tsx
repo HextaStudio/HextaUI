70d04f388063dee6ffa5f10f2f2a66f2345b7c98
preetsuthar17 <preetsutharxd@gmail.com>
2024-05-27 13:50:51 +0530

    feat: Update Testimonials component to include additional testimonials and reverse animation direction

--- src/components/landingPage/Testimonials.tsx
+++ src/components/landingPage/Testimonials.tsx
@@ -17,12 +17,11 @@ export const Testimonials = () => {
         <div className="testimonial-item item4">
           <Tweet id="1787091751997440143" />
         </div>
-        <div className="testimonial-item item4">
-          <Tweet id="1787091751997440143" />
-        </div>
         <div className="testimonial-item item5">
           <Tweet id="1786800646433423488" />
         </div>
+      </div>
+      <div className="flex gap-4 testimonial-wrapper-reverse">
         <div className="testimonial-item item6">
           <Tweet id="1785345519033684406" />
         </div>
@@ -31,6 +30,12 @@ export const Testimonials = () => {
         </div>
         <div className="testimonial-item item8">
           <Tweet id="1787088566025122186" />
+        </div>
+        <div className="testimonial-item item9">
+          <Tweet id="1794991372996313366" />
+        </div>
+        <div className="testimonial-item item10">
+          <Tweet id="1794994587213451321" />
         </div>
       </div>
     </>
