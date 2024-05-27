c63e33bffb3f430452fd2cf3712dbcdeafbd5fbe
preetsuthar17 <preetsutharxd@gmail.com>
2024-05-27 09:00:26 +0530

    feat: Add Countdown component to components.json

---
+++ src/components/hexta-ui/Countdown.tsx
@@ -0,0 +1,98 @@
+import { MotionValue, motion, useSpring, useTransform } from "framer-motion";
+import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx";
+
+const cn = (...args: any[]) => {
+  return twMerge(clsx(args));
+};
+
+const fontSize = 40;
+const padding = 10;
+const height = fontSize + padding;
+
+interface CounterProps {
+  start?: number;
+  end: number;
+  duration?: number;
+  className?: string;
+  fontSize?: number;
+}
+
+export const Countdown = ({
+  start = 0,
+  end,
+  duration = end,
+  className,
+  fontSize = 30,
+}: CounterProps) => {
+  const [value, setValue] = useState(start);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (value < end) {
+        setValue((prev) => prev + 1);
+      }
+    }, (duration / (end - start)) * 1000);
+
+    return () => clearInterval(interval);
+  }, [value]);
+
+  return (
+    <div
+      style={{ fontSize }}
+      className={cn(
+        "flex overflow-hidden rounded px-2 leading-none text-white font-bold ",
+        className
+      )}
+    >
+      {value >= 100000 && <Digit place={100000} value={value} />}
+      {value >= 10000 && <Digit place={10000} value={value} />}
+      {value >= 1000 && <Digit place={1000} value={value} />}
+      {value >= 100 && <Digit place={100} value={value} />}
+      {value >= 10 && <Digit place={10} value={value} />}
+      <Digit place={1} value={value} />
+    </div>
+  );
+};
+
+function Digit({ place, value }: { place: number; value: number }) {
+  let valueRoundedToPlace = Math.floor(value / place);
+  let animatedValue = useSpring(valueRoundedToPlace);
+
+  useEffect(() => {
+    animatedValue.set(valueRoundedToPlace);
+  }, [animatedValue, valueRoundedToPlace]);
+
+  return (
+    <div style={{ height }} className="relative w-[1ch] tabular-nums">
+      {[...Array(10)].map((_, i) => (
+        <Number key={i} mv={animatedValue} number={i} />
+      ))}
+    </div>
+  );
+}
+
+function Number({ mv, number }: { mv: MotionValue; number: number }) {
+  let y = useTransform(mv, (latest) => {
+    let placeValue = latest % 10;
+    let offset = (10 + number - placeValue) % 10;
+
+    let memo = offset * height;
+
+    if (offset > 5) {
+      memo -= 10 * height;
+    }
+
+    return memo;
+  });
+
+  return (
+    <motion.span
+      style={{ y }}
+      className="absolute inset-0 flex items-center justify-center"
+    >
+      {number}
+    </motion.span>
+  );
+}
---
+++ src/pages/docs/components/layout/Countdown.tsx
@@ -0,0 +1,83 @@
+import { CodeBlock } from "@/components/DocsPage/CodeBlock";
+import { DocsHeader } from "@/components/DocsPage/DocsHeader";
+import { DocsLayout } from "@/components/DocsPage/DocsLayout";
+import { DocsPreview } from "@/components/DocsPage/DocsPreview";
+import { InstallationSteps } from "@/components/DocsPage/InstallationSteps";
+import { Countdown } from "@/components/hexta-ui/Countdown";
+import { Table } from "@/components/hexta-ui/Table";
+
+const columns = [
+  {
+    label: "Prop Name",
+    key: "propName",
+  },
+  {
+    label: "Prop Type",
+    key: "propType",
+  },
+  {
+    label: "Default Value",
+    key: "defaultValue",
+  },
+  {
+    label: "Description",
+    key: "description",
+  },
+];
+
+const data = [
+  {
+    propName: "end",
+    propType: "Number",
+    defaultValue: "Required",
+    description: "The end value of the countdown",
+  },
+  {
+    propName: "duration",
+    propType: "Number",
+    defaultValue: "end",
+    description: "The duration of the countdown",
+  },
+  {
+    propName: "className",
+    propType: "String",
+    defaultValue: "null",
+    description: "Additional classes to be added",
+  },
+  {
+    propName: "fontSize",
+    propType: "Number",
+    defaultValue: "30",
+    description: "Font size of the countdown",
+  },
+];
+
+const countdown = () => {
+  return (
+    <>
+      <DocsLayout>
+        <main>
+          <DocsHeader
+            title="Countdown"
+            description="A simple animated countdown component."
+          />
+          <DocsPreview>
+            <Countdown end={50} duration={20} />
+          </DocsPreview>
+          <InstallationSteps
+            imports={["countdown"]}
+            component={["countdown"]}
+          />
+          <CodeBlock
+            lang="tsx"
+            filename="tsx"
+            code={`<Countdown end={50} duration={20} />`}
+          />
+          <Table columns={columns} data={data} tableTitle="Props Information" />
+        </main>
+      </DocsLayout>
+    </>
+  );
+};
+
+export default countdown;
