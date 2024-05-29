fa10db650788c5f34f3ffd5484a720a60f61deef
preetsuthar17 <preetsutharxd@gmail.com>
2024-05-29 12:30:25 +0530

    chore: Update Hero component with new heading and icons

--- src/components/landingPage/Hero.tsx
+++ src/components/landingPage/Hero.tsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
+import { FaReact } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { TbBrandFramerMotion } from "react-icons/tb";
 
 export const Hero = () => {
   return (
     <>
       <div className="left-1/2 -translate-x-1/2 hero-glow  fading-out-2 absolute top-0"></div>
       <section
-        className="flex items-center justify-center flex-col pt-[14rem] px-[1rem]relative max-w-full  overflow-hidden backdrop-blur-[6rem]"
+        className="flex items-center justify-center flex-col pt-[12rem] px-[1rem] relative max-w-full  overflow-hidden backdrop-blur-[6rem] z-[999]"
         style={{
           perspective: "1000px",
         }}
@@ -18,8 +22,8 @@ export const Hero = () => {
           }}
         ></div> */}
         <div className="flex flex-col items-center justify-center gap-4 text-center px-4">
-          <h1 className="text-7xl font-bold tracking-tight max-w-[60rem] max-[482px]:text-6xl max-[372px]:text-5xl relative">
-            Level up your Design Game with HextaUI
+          <h1 className="text-7xl font-bold tracking-tight max-w-[60rem] max-[482px]:text-6xl max-[372px]:text-5xl relative hero-heading">
+            Modern & Stunning Website Templates and Examples
           </h1>
           <p className="text-md opacity-80 max-w-[40rem] w-full relative">
             Easy to use copy and paste components for your website. Make your
@@ -32,9 +36,18 @@ export const Hero = () => {
             >
               Browse Components
             </Link>
-            <Link href="#pricing" className="rounded-md secondary-button grow">
+            <Link
+              href="https://pro.ui.hextastudio.in/pricing"
+              className="rounded-md secondary-button grow"
+            >
               Pricing
             </Link>
+          </div>{" "}
+          <div className="flex gap-4 opacity-80">
+            <FaReact size={24} />
+            <SiTypescript size={24} />
+            <RiTailwindCssFill size={24} />
+            <TbBrandFramerMotion size={24} />
           </div>
         </div>
       </section>
