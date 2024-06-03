756ef87662370993be1203670823edf7fc7c1676
preetsuthar17 <preetsutharxd@gmail.com>
2024-06-03 11:33:48 +0530

    refactor: Optimize Testimonials component with lazy loading for images

--- src/components/landingPage/Testimonials.tsx
+++ src/components/landingPage/Testimonials.tsx
@@ -10,39 +10,60 @@ export const Testimonials = () => {
           about Magic UI on X.
         </p>
       </div>
-      <div className="flex flex-col -space-y-4">
-        <div className="flex gap-4 testimonial-wrapper scale-[0.9]">
-          <div className="testimonial-item item1">
-            <Tweet id="1782653978683072684" />
+
+      <div className="group flex overflow-hidden my-[3rem] py-2 [--gap:1rem] [gap:var(--gap)] flex-row max-w-full [--duration:40s] [mask-image:linear-gradient(to_right,_rgba(0,_0,_0,_0),rgba(0,_0,_0,_1)_10%,rgba(0,_0,_0,_1)_90%,rgba(0,_0,_0,_0))]">
+        <div className="flex flex-col [gap:var(--gap)] ">
+          <div className="flex [gap:var(--gap)] h-[300px] overflow-hidden [mask-image:linear-gradient(to_top,_rgba(0,_0,_0,_0),rgba(0,_0,_0,_1)_10%,rgba(0,_0,_0,_1)_90%,rgba(0,_0,_0,_0))]">
+            {Array(4)
+              .fill(0)
+              .map((_, i) => (
+                <div
+                  className="flex shrink-0 justify-around [gap:var(--gap)] animate-marquee flex-row"
+                  key={i}
+                >
+                  <div className="testimonial-item scale-[0.9] text-sm w-[330px] item1">
+                    <Tweet id="1782653978683072684" />
+                  </div>
+                  <div className="testimonial-item scale-[0.9] text-sm w-[330px] item2">
+                    <Tweet id="1782680701067776207" />
+                  </div>
+                  <div className="testimonial-item scale-[0.9] text-sm w-[330px] item3">
+                    <Tweet id="1787135072392364394" />
+                  </div>
+                  <div className="testimonial-item scale-[0.9] text-sm w-[330px] item4">
+                    <Tweet id="1787091751997440143" />
+                  </div>
+                  <div className="testimonial-item scale-[0.9] text-sm w-[330px] item5">
+                    <Tweet id="1786800646433423488" />
+                  </div>
+                </div>
+              ))}
           </div>
-          <div className="testimonial-item item2">
-            <Tweet id="1782680701067776207" />
-          </div>
-          <div className="testimonial-item item3">
-            <Tweet id="1787135072392364394" />
-          </div>
-          <div className="testimonial-item item4">
-            <Tweet id="1787091751997440143" />
-          </div>
-          <div className="testimonial-item item5">
-            <Tweet id="1786800646433423488" />
-          </div>
-        </div>
-        <div className="flex gap-4 testimonial-wrapper-reverse mb-8 scale-[0.9]">
-          <div className="testimonial-item item6">
-            <Tweet id="1785345519033684406" />
-          </div>
-          <div className="testimonial-item item7">
-            <Tweet id="1787163815987007863" />
-          </div>
-          <div className="testimonial-item item8">
-            <Tweet id="1787088566025122186" />
-          </div>
-          <div className="testimonial-item item9">
-            <Tweet id="1794991372996313366" />
-          </div>
-          <div className="testimonial-item item10">
-            <Tweet id="1794994587213451321" />
+          <div className="flex [gap:var(--gap)] h-[300px] overflow-hidden [mask-image:linear-gradient(to_top,_rgba(0,_0,_0,_0),rgba(0,_0,_0,_1)_10%,rgba(0,_0,_0,_1)_90%,rgba(0,_0,_0,_0))]">
+            {Array(4)
+              .fill(0)
+              .map((_, i) => (
+                <div
+                  className="flex shrink-0 justify-around [animation-direction:reverse] [gap:var(--gap)] animate-marquee flex-row"
+                  key={i}
+                >
+                  <div className="testimonial-item scale-[0.9] text-sm w-[330px] item6">
+                    <Tweet id="1785345519033684406" />
+                  </div>
+                  <div className="testimonial-item scale-[0.9] text-sm w-[330px] item7">
+                    <Tweet id="1787163815987007863" />
+                  </div>
+                  <div className="testimonial-item scale-[0.9] text-sm w-[330px] item8">
+                    <Tweet id="1787088566025122186" />
+                  </div>
+                  <div className="testimonial-item scale-[0.9] text-sm w-[330px] item9">
+                    <Tweet id="1794991372996313366" />
+                  </div>
+                  <div className="testimonial-item scale-[0.9] text-sm w-[330px] item10">
+                    <Tweet id="1794994587213451321" />
+                  </div>
+                </div>
+              ))}
           </div>
         </div>
       </div>
