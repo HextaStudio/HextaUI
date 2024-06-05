2f7d119cf4b55fe85b078c224b3f2fa3083199cd
preetsuthar17 <preetsutharxd@gmail.com>
2024-06-05 08:15:58 +0530

    feat: Optimize Testimonials component with lazy loading for images

---
+++ src/pages/docs/examples/general/Globe.tsx
@@ -0,0 +1,301 @@
+import { DocsHeader } from "@/components/DocsPage/DocsHeader";
+import { DocsLayout } from "@/components/DocsPage/DocsLayout";
+import { DocsPreview } from "@/components/DocsPage/DocsPreview";
+import { CodeBlock } from "@/components/DocsPage/CodeBlock";
+
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const cn = (...args: any[]) => {
+  return twMerge(clsx(args));
+};
+
+import createGlobe, { COBEOptions } from "cobe";
+import { useCallback, useEffect, useRef } from "react";
+import { useSpring } from "react-spring";
+import { DocsSEO } from "@/components/DocsPage/DocsSEO";
+
+const GLOBE_CONFIG: COBEOptions = {
+  width: 800,
+  height: 800,
+  onRender: () => {},
+  devicePixelRatio: 2,
+  phi: 0,
+  theta: 0.3,
+  dark: 0,
+  diffuse: 0.4,
+  mapSamples: 16000,
+  mapBrightness: 1,
+  baseColor: [1, 1, 2],
+  markerColor: [251 / 255, 100 / 255, 21 / 255],
+  glowColor: [1, 1, 2],
+  markers: [
+    {
+      location: [20.5937, 78.9629],
+      size: 0.1,
+    },
+  ],
+};
+
+export const Globe = ({
+  className,
+  config = GLOBE_CONFIG,
+}: {
+  className?: string;
+  config?: COBEOptions;
+}) => {
+  let phi = 0;
+  let width = 0;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const pointerInteracting = useRef(null);
+  const pointerInteractionMovement = useRef(0);
+  const [{ r }, api] = useSpring(() => ({
+    r: 0,
+    config: {
+      mass: 1,
+      tension: 280,
+      friction: 40,
+      precision: 0.001,
+    },
+  }));
+
+  const updatePointerInteraction = (value: any) => {
+    pointerInteracting.current = value;
+    canvasRef.current!.style.cursor = value ? "grabbing" : "grab";
+  };
+
+  const updateMovement = (clientX: any) => {
+    if (pointerInteracting.current !== null) {
+      const delta = clientX - pointerInteracting.current;
+      pointerInteractionMovement.current = delta;
+      api.start({ r: delta / 200 });
+    }
+  };
+
+  const onRender = useCallback(
+    (state: Record<string, any>) => {
+      if (!pointerInteracting.current) phi += 0.005;
+      state.phi = phi + r.get();
+      state.width = width * 2;
+      state.height = width * 2;
+    },
+    [pointerInteracting, phi, r]
+  );
+
+  const onResize = () => {
+    if (canvasRef.current) {
+      width = canvasRef.current.offsetWidth;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+    onResize();
+
+    const globe = createGlobe(canvasRef.current!, {
+      ...config,
+      width: width * 2,
+      height: width * 2,
+      onRender,
+    });
+
+    setTimeout(() => (canvasRef.current!.style.opacity = "1"));
+    return () => globe.destroy();
+  }, []);
+
+  return (
+    <div
+      className={cn(
+        "absolute inset-0 mx-auto aspect-[1/1] w-full max-w-[600px]",
+        className
+      )}
+    >
+      <canvas
+        className={cn(
+          "h-full w-full opacity-0 transition-opacity duration-500 [contain:layout_paint_size]"
+        )}
+        ref={canvasRef}
+        onPointerDown={(e) =>
+          updatePointerInteraction(
+            e.clientX - pointerInteractionMovement.current
+          )
+        }
+        onPointerUp={() => updatePointerInteraction(null)}
+        onPointerOut={() => updatePointerInteraction(null)}
+        onMouseMove={(e) => updateMovement(e.clientX)}
+        onTouchMove={(e) =>
+          e.touches[0] && updateMovement(e.touches[0].clientX)
+        }
+      />
+    </div>
+  );
+};
+
+const globe = () => {
+  return (
+    <>
+      <DocsSEO
+        title="3D Globe - hextastudio/ui"
+        description="A 3D globe animated globe component."
+        image="https://i.imgur.com/jTNOHrS.png"
+      />
+      <DocsLayout>
+        <main>
+          <DocsHeader
+            title="3D Globe"
+            description="A 3D globe animated globe component."
+          />
+          <DocsPreview className="min-h-[37rem]">
+            <Globe />
+          </DocsPreview>
+          <CodeBlock
+            lang="bash"
+            filename="bash"
+            free
+            code={`npm install -D cobe react-spring`}
+          />
+          <CodeBlock
+            lang="tsx"
+            filename="tsx"
+            free
+            code={`import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+const cn = (...args: any[]) => {
+  return twMerge(clsx(args));
+};
+
+import createGlobe, { COBEOptions } from "cobe";
+import { useCallback, useEffect, useRef } from "react";
+import { useSpring } from "react-spring";
+
+const GLOBE_CONFIG: COBEOptions = {
+  width: 800,
+  height: 800,
+  onRender: () => {},
+  devicePixelRatio: 2,
+  phi: 0,
+  theta: 0.3,
+  dark: 0,
+  diffuse: 0.4,
+  mapSamples: 16000,
+  mapBrightness: 1,
+  baseColor: [1, 1, 2],
+  markerColor: [251 / 255, 100 / 255, 21 / 255],
+  glowColor: [1, 1, 2],
+  markers: [
+    {
+      location: [20.5937, 78.9629],
+      size: 0.1,
+    },
+  ],
+};
+
+export const Globe = ({
+  className,
+  config = GLOBE_CONFIG,
+}: {
+  className?: string;
+  config?: COBEOptions;
+}) => {
+  let phi = 0;
+  let width = 0;
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const pointerInteracting = useRef(null);
+  const pointerInteractionMovement = useRef(0);
+  const [{ r }, api] = useSpring(() => ({
+    r: 0,
+    config: {
+      mass: 1,
+      tension: 280,
+      friction: 40,
+      precision: 0.001,
+    },
+  }));
+
+  const updatePointerInteraction = (value: any) => {
+    pointerInteracting.current = value;
+    canvasRef.current!.style.cursor = value ? "grabbing" : "grab";
+  };
+
+  const updateMovement = (clientX: any) => {
+    if (pointerInteracting.current !== null) {
+      const delta = clientX - pointerInteracting.current;
+      pointerInteractionMovement.current = delta;
+      api.start({ r: delta / 200 });
+    }
+  };
+
+  const onRender = useCallback(
+    (state: Record<string, any>) => {
+      if (!pointerInteracting.current) phi += 0.005;
+      state.phi = phi + r.get();
+      state.width = width * 2;
+      state.height = width * 2;
+    },
+    [pointerInteracting, phi, r]
+  );
+
+  const onResize = () => {
+    if (canvasRef.current) {
+      width = canvasRef.current.offsetWidth;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", onResize);
+    onResize();
+
+    const globe = createGlobe(canvasRef.current!, {
+      ...config,
+      width: width * 2,
+      height: width * 2,
+      onRender,
+    });
+
+    setTimeout(() => (canvasRef.current!.style.opacity = "1"));
+    return () => globe.destroy();
+  }, []);
+
+  return (
+    <div
+      className={cn(
+        "absolute inset-0 mx-auto aspect-[1/1] w-full max-w-[600px]",
+        className
+      )}
+    >
+      <canvas
+        className={cn(
+          "h-full w-full opacity-0 transition-opacity duration-500 [contain:layout_paint_size]"
+        )}
+        ref={canvasRef}
+        onPointerDown={(e) =>
+          updatePointerInteraction(
+            e.clientX - pointerInteractionMovement.current
+          )
+        }
+        onPointerUp={() => updatePointerInteraction(null)}
+        onPointerOut={() => updatePointerInteraction(null)}
+        onMouseMove={(e) => updateMovement(e.clientX)}
+        onTouchMove={(e) =>
+          e.touches[0] && updateMovement(e.touches[0].clientX)
+        }
+      />
+    </div>
+  );
+};`}
+          />
+          <CodeBlock
+            lang="tsx"
+            free
+            filename="tsx"
+            code={`<Globe/>`}
+            title="Usage"
+          />
+        </main>
+      </DocsLayout>
+    </>
+  );
+};
+
+export default globe;
